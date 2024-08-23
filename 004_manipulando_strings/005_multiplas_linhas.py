bf9d9239ec6b35e5b297cfe85c9b9c5ecf154d75
sahrarafaela <sararafaela704@gmail.com>
2024-08-23 20:04:43 -0300

    att arquivo

--- 004_manipulando_strings/005_multiplas_linhas.py
+++ 004_manipulando_strings/005_multiplas_linhas.py
@@ -17,3 +17,16 @@ Essa mensagem tem diferentes recuos
 '''
 
 print(mensagem)
+
+
+print(
+    """
+==========MENU==========
+1 - Depositar
+2 - Sacar
+0 - Sair
+
+===========================
+Obrigada por usar nosso sistema!!!
+"""
+)
